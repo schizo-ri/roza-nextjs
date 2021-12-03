6f1fff58d0de614d29e98012471316925f5b5c3d
schizo-ri <edin.lockedin@gmail.com>
2021-12-03 14:43:07 +0100

    add more css variables, apply them to home page style

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -52,7 +52,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template& utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -61,5 +61,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
